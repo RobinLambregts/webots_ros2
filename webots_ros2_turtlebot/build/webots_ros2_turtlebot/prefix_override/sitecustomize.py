eada9423de1146a6b6ccfe1c03418e3695a90dd4
RobinLambregts <robin.lambregts@student.uhasselt.be>
2025-12-19 17:33:02 +0100

    ik weet hoe robot laten rondrijden + navigatie, wodeeeeee

---
+++ webots_ros2_turtlebot/build/webots_ros2_turtlebot/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/mnt/c/Users/robin/Documents/WEBOTS_SHIT/webots_ws/src/webots_ros2/webots_ros2_turtlebot/install/webots_ros2_turtlebot'
